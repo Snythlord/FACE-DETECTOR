31af6341e056ef69d13e351ad1cf3dfb857ae327
Snythlord <96640521+Snythlord@users.noreply.github.com>
2023-02-03 12:56:31 -0500

    Made ai face detector

--- FACE_DETECTOR.py
+++ FACE_DETECTOR.py
@@ -3,10 +3,25 @@ import cv2
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 # This is to choose an image to detect faces on
-img = cv2.imread("TC.png")
+#img = cv2.imread("TC.png")
+webcam = cv2.VideoCapture(0)
+while True:
+    successful_frame_read, frame = webcam.read()
 
-#Grayscale the img
-cv2.imshow('Clever Program Face Detector', img)
-cv2.waitKey()
 
-print("CODE COMPLETED")
+    grayscaled = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    face_coordinates = trained_face_data.detectMultiScale(grayscaled)
+
+    for (x, y, w, h) in face_coordinates:
+        cv2.rectangle(frame,(x, y), (x+w, y+h), (0, 255, 0), 3)
+    cv2.imshow('Face Detector', frame)
+
+
+    key = cv2.waitKey(1)
+    if key ==81 or key ==113:
+        break
+
+webcam.release()
+    
+
